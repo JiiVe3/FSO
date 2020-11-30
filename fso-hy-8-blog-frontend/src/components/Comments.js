1bfa8086100ffe594134c9d946a1f2e7d066b68b
Janne Vehniäinen <janne.vehniainen@gmail.com>
2020-11-30 17:13:36 +0200

    Chapter 7 exercise progress

--- fso-hy-8-blog-frontend/src/components/Comments.js
+++ fso-hy-8-blog-frontend/src/components/Comments.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const Comments = () => {
+const Comments = ({ blog }) => {
+    const addComment = (newComment) => {
+
+    }
+    
     return (
         <>
         </>
